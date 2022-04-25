33e047a1459320574bafcc30a61f660d60393bb3
PeterDing <dfhayst@gmail.com>
2022-04-25 10:20:30 +0800

    Add route function to generate all routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -8,3 +8,21 @@ mod utils;
 pub use health_check::health_check;
 pub use login::login;
 pub use search::search;
+
+use actix_web::web;
+
+pub fn route() -> actix_web::Scope {
+    web::scope("")
+        .route("/health_check", web::get().to(health_check))
+        .route("/login", web::post().to(login))
+        .route("/search", web::get().to(search))
+        .route("/albums", web::get().to(albums::albums))
+        .route("/albums/{id}/tracks", web::get().to(albums::album_tracks))
+        .route("/me/albums", web::get().to(albums::saved_albums))
+        .route("/me/albums", web::put().to(albums::save_albums))
+        .route("/me/albums", web::delete().to(albums::delete_albums))
+        .route(
+            "/browse/new-releases",
+            web::delete().to(albums::new_releases),
+        )
+}
