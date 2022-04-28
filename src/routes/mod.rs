0f548e28150613bd4b6d495644e594ce0a8f42e7
PeterDing <dfhayst@gmail.com>
2022-04-28 14:09:33 +0800

    Add routes
    * `/tracks/{id}`
    * `/albums/{id}`
    * `/episodes/{id}`
    * `/shows/{id}`
    * `/audio/{id}`

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,6 @@
 mod albums;
 mod artists;
+mod audios;
 mod auth;
 mod episodes;
 mod health_check;
@@ -7,6 +8,7 @@ mod login;
 mod params;
 mod search;
 mod shows;
+mod tracks;
 mod utils;
 
 pub use health_check::health_check;
@@ -24,6 +26,7 @@ pub fn route() -> actix_web::Scope {
         // Search api
         .route("/search", web::get().to(search))
         // Albums apis
+        .route("/albums/{id}", web::get().to(albums::album))
         .route("/albums", web::get().to(albums::albums))
         .route("/albums/{id}/tracks", web::get().to(albums::album_tracks))
         .route("/me/albums", web::get().to(albums::saved_albums))
@@ -31,6 +34,7 @@ pub fn route() -> actix_web::Scope {
         .route("/me/albums", web::delete().to(albums::delete_albums))
         .route("/browse/new-releases", web::get().to(albums::new_releases))
         // Artists apis
+        .route("/artists/{id}", web::get().to(artists::artist))
         .route("/artists", web::get().to(artists::artists))
         .route(
             "/artists/{id}/albums",
@@ -45,14 +49,34 @@ pub fn route() -> actix_web::Scope {
             web::get().to(artists::artist_related_artists),
         )
         // Shows apis
+        .route("/shows/{id}", web::get().to(shows::show))
         .route("/shows", web::get().to(shows::shows))
         .route("/shows/{id}/episodes", web::get().to(shows::show_episodes))
         .route("/me/shows", web::get().to(shows::saved_shows))
         .route("/me/shows", web::put().to(shows::save_shows))
         .route("/me/shows", web::delete().to(shows::delete_shows))
         // Episodes apis
+        .route("/episodes/{id}", web::get().to(episodes::episode))
         .route("/episodes", web::get().to(episodes::episodes))
         .route("/me/episodes", web::get().to(episodes::saved_episodes))
         .route("/me/episodes", web::put().to(episodes::save_episodes))
         .route("/me/episodes", web::delete().to(episodes::delete_episodes))
+        // Tracks apis
+        .route("/tracks/{id}", web::get().to(tracks::track))
+        .route("/tracks", web::get().to(tracks::tracks))
+        .route("/me/tracks", web::get().to(tracks::saved_tracks))
+        .route("/me/tracks", web::put().to(tracks::save_tracks))
+        .route("/me/tracks", web::delete().to(tracks::delete_tracks))
+        .route(
+            "/audio-features/{id}",
+            web::get().to(tracks::track_features),
+        )
+        .route("/audio-features", web::get().to(tracks::tracks_features))
+        .route(
+            "/audio-analysis/{id}",
+            web::get().to(tracks::track_analysis),
+        )
+        // Audios apis
+        .route("/audio/{id}", web::get().to(audios::audio))
+        .route("/audio-stream/{id}", web::get().to(audios::audio_stream))
 }
